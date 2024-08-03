54fecce35103a2b41d12df23a9829bd8e3a0ed74
Caleb Saiia <calebsaiia@gmail.com>
2024-08-03 13:55:55 -0500

    added Apollo Server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const typeDefs = `
+  type Profile {
+    _id: ID
+    name: String
+    email: String
+    skills: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+    me: Profile
+  }
+
+  type Mutation {
+    addProfile(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    
+    addSkill(profileId: ID!, skill: String!): Profile
+    removeProfile: Profile
+    removeSkill(skill: String!): Profile
+  }
+`;
+
+module.exports = typeDefs;
